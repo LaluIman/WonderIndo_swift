82a31b1b3ffa651aa73b29277993fc1b9a961afb
Eye-Mans <laluiman468@gmail.com>
2024-04-21 13:48:47 +0700

    udpt

--- Wonderful Indonesia/component/HoriView.swift
+++ Wonderful Indonesia/component/HoriView.swift
@@ -11,35 +11,60 @@ struct HoriView: View {
     var body: some View {
         ScrollView(.horizontal, showsIndicators: false){
             HStack {
-                Image("borobudur")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 250, height: 150)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+                VStack {
+                    Image("borobudur")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 250, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    Text("Candi Borobudur")
+                        .font(.title2).bold()
+                    .frame(width: 250, alignment: .leading)
+                }
                 
-                Image("komodo")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 250, height: 150)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+                VStack {
+                    Image("komodo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 250, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    Text("Pulau Komodo")
+                        .font(.title2).bold()
+                    .frame(width: 250, alignment: .leading)
+                }
                 
-                Image("bromo")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 250, height: 150)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+                VStack {
+                    Image("bromo")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 250, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    Text("Bromo")
+                        .font(.title2).bold()
+                    .frame(width: 250, alignment: .leading)
+                }
                 
-                Image("prambanan")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 250, height: 150)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+                VStack {
+                    Image("prambanan")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 250, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    Text("Candi Prambanan")
+                        .font(.title2).bold()
+                    .frame(width: 250, alignment: .leading)
+                }
                 
-                Image("ampat")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 250, height: 150)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
+                VStack {
+                    Image("ampat")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 250, height: 150)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    Text("Raja Ampat")
+                        .font(.title2).bold()
+                    .frame(width: 250, alignment: .leading)
+                }
             }
             .padding(.horizontal)
         }
